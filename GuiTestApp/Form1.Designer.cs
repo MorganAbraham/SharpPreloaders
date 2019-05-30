ca0b806c62ddc0c92866541ee00c7717871cc3ac
Morgan Abraham <mabraham@paychex.com>
2019-05-30 13:15:29 -0400

    Added buttons and comboBoxes to the GuiTestApp form to be able to test the GUI by changing the speed and animation as well as starting, stopping, and pausing the animation

--- GuiTestApp/Form1.Designer.cs
+++ GuiTestApp/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.comboStyle = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboSpeed = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(39, 159);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 48);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Animation";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(39, 294);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(96, 48);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop Animation";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(39, 231);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(96, 48);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause Animation";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // comboStyle
+            // 
+            this.comboStyle.FormattingEnabled = true;
+            this.comboStyle.Location = new System.Drawing.Point(17, 27);
+            this.comboStyle.Name = "comboStyle";
+            this.comboStyle.Size = new System.Drawing.Size(159, 21);
+            this.comboStyle.TabIndex = 3;
+            this.comboStyle.SelectedIndexChanged += new System.EventHandler(this.comboStyle_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.comboSpeed);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboStyle);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnPause);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Location = new System.Drawing.Point(12, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(189, 396);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Animation Style";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Animation Speed";
+            // 
+            // comboSpeed
+            // 
+            this.comboSpeed.FormattingEnabled = true;
+            this.comboSpeed.Location = new System.Drawing.Point(17, 81);
+            this.comboSpeed.Name = "comboSpeed";
+            this.comboSpeed.Size = new System.Drawing.Size(159, 21);
+            this.comboSpeed.TabIndex = 5;
+            this.comboSpeed.SelectedIndexChanged += new System.EventHandler(this.comboSpeed_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.ComboBox comboStyle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboSpeed;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GuiTestApp/Form1.cs
+++ GuiTestApp/Form1.cs
@@ -6,14 +6,84 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SharpPreloaders;
 
 namespace GuiTestApp
 {
     public partial class Form1 : Form
     {
+        private Preloader preloader;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dictionary<string, Preloader.AnimationSpeed> speeds = new Dictionary<string, Preloader.AnimationSpeed>();
+            foreach(Preloader.AnimationSpeed animationSpeed in Enum.GetValues(typeof(Preloader.AnimationSpeed)))
+            {
+                speeds.Add(animationSpeed.ToString(), animationSpeed);
+            }
+
+            comboSpeed.DataSource = new BindingSource(speeds, null);
+            comboSpeed.DisplayMember = "Key";
+            comboSpeed.ValueMember = "Value";
+            comboSpeed.SelectedIndex = -1;
+
+            Dictionary<string, Preloader.AnimationStyle> styles = new Dictionary<string, Preloader.AnimationStyle>();
+            foreach (Preloader.AnimationStyle animationStyle in Enum.GetValues(typeof(Preloader.AnimationStyle)))
+            {
+                styles.Add(animationStyle.ToString(), animationStyle);
+            }
+
+            comboStyle.DataSource = new BindingSource(styles, null);
+            comboStyle.DisplayMember = "Key";
+            comboStyle.ValueMember = "Value";
+            comboStyle.SelectedIndex = -1;
+            
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            preloader.StartAnimation();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            preloader.StopAnimation();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            preloader.PauseAnimation();
+        }
+
+        private void comboStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (preloader != null)
+            {
+                preloader.StopAnimation();
+            }
+            if (this.comboSpeed.SelectedIndex != -1 && this.comboStyle.SelectedIndex != -1)
+            {
+                preloader = new Preloader(this, (Preloader.AnimationStyle)this.comboStyle.SelectedValue, (Preloader.AnimationSpeed)this.comboSpeed.SelectedValue);
+                preloader.StartAnimation();
+            }
+        }
+
+        private void comboSpeed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(preloader != null)
+            {
+                preloader.StopAnimation();
+            }
+            if (this.comboSpeed.SelectedIndex != -1 && this.comboStyle.SelectedIndex != -1)
+            {
+                preloader = new Preloader(this, (Preloader.AnimationStyle)this.comboStyle.SelectedValue, (Preloader.AnimationSpeed)this.comboSpeed.SelectedValue);
+                preloader.StartAnimation();
+            }
         }
     }
 }
